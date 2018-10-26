33c880c53ea3c5f0de4773cdadc5eae6821023ec
Justin <justin.maliakal@tatcha.com>
2018-10-26 12:15:24 +0530

    limit values changed to 250

--- api/config/params.php
+++ api/config/params.php
@@ -3,9 +3,9 @@
 return [
     'adminEmail' => 'admin@example.com',
     'type_limit'=>[
-        'TYPE1'=>500,
+        'TYPE1'=>250,
         'TYPE2'=>250,
-        'TYPE3'=>150,
-        'TYPE4'=>100,
+        'TYPE3'=>250,
+        'TYPE4'=>250,
     ]
 ];
